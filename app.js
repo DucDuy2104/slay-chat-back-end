7f14b5529510d8d7ae8e0957989137f5487ac468
DucDuy2104 <117710024+DucDuy2104@users.noreply.github.com>
2024-07-18 17:10:12 +0700

    new app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(process.env.PORT || 4444, () => {
+    console.log(`Server is running on port ${process.env.PORT || 4444}`);
+})
